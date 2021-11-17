7c0061dedea098a2dc134b4a4c647e6db02e358a
Sergiu Apostul <sergiu.apostul@gmail.com>
2021-11-17 04:52:39 +0200

    Pass Down Search Result and Render Result List

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -6,9 +6,11 @@ class TrackList extends React.Component {
     render(){
         return (
             <div className="TrackList">
-                <Track />
-                <Track />
-                <Track />
+                {
+                    this.props.tracks.map(track => {
+                        return <Track key={track.id} track={track} />;
+                    })
+                }
             </div>
         );
     }
